48c04c5cc58f26b999fd99dafd44f360bb8c24d2
Peter McCready <petermccready2@gmail.com>
2017-11-12 11:29:20 +0000

    Adds Home Activity and delayed transition to Main Activity

--- app/src/main/java/com/example/peter/toptrumps/Game.java
+++ app/src/main/java/com/example/peter/toptrumps/Game.java
@@ -3,8 +3,6 @@ package com.example.peter.toptrumps;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 
 /**
--- app/src/main/java/com/example/peter/toptrumps/HomeActivity.java
+++ app/src/main/java/com/example/peter/toptrumps/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.peter.toptrumps;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
+
     }
 }
