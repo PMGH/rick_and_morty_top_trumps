376fe305451a3ab8249a70b540a675cf8d0a70c5
Peter McCready <petermccready2@gmail.com>
2017-11-14 18:38:19 +0000

    Refactors ResultActivity

--- app/src/main/java/com/example/peter/toptrumps/ResultActivity.java
+++ app/src/main/java/com/example/peter/toptrumps/ResultActivity.java
@@ -53,6 +53,41 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        //get extras
+        handleExtras();
+
+        // setup game
+        game = Game.getInstance();
+        userCard = game.getPlayer1().getTopCard();
+        cpuCard = game.getDealer().getTopCard();
+
+        // setup views
+        getLayoutViews();
+        setLayoutViews();
+
+        // play round and get winner details
+        game.playRound(category);
+        isRoundDraw = game.isRoundDraw();
+        roundWinnerName = game.getRoundWinner().getName();
+        roundWinningCard = game.getRoundWinningCard();
+        roundWinnerScore = game.getRoundWinningScore();
+
+        winDeclaration();
+
+        // check if draw then set colours accordingly
+        if (isRoundDraw){
+            setDrawColours();
+            // draw declaration
+            String roundDrawDeclarationTextStr = "DRAW! Cards added to the pile.";
+            roundWinDeclarationText.setText(roundDrawDeclarationTextStr);
+        } else {
+            setWinColours();
+            toastCatchphrase();
+        }
+
+    }
+
+    public void handleExtras(){
         Intent resultActivityIntent = getIntent();
 
         if (resultActivityIntent.hasExtra("category")){
@@ -64,103 +99,62 @@ public class ResultActivity extends AppCompatActivity {
             game = Game.getInstance();
             category = game.getCPUBestCategory();
         }
+    }
 
-        game = Game.getInstance();
-
-        userCard = game.getPlayer1().getTopCard();
-        cpuCard = game.getDealer().getTopCard();
-
+    public void getLayoutViews(){
         // page title text
         roundResultText = (TextView) findViewById(R.id.round_result_title_text);
-        String roundResultTextStr = "Round " + game.getRoundNumber() + " Result";
-        roundResultText.setText(roundResultTextStr);
-
-        // cpu
-        cpuNameText = (TextView) findViewById(R.id.cpu_name_text);
-        cpuNameText.setText(game.getDealer().getName());
-        cpuBackplate = (ImageView) findViewById(R.id.cpu_top_card_backplate);
-        cpuTopCardName = (TextView) findViewById(R.id.cpu_top_card_name_text);
-        cpuTopCardName.setText(game.getDealer().getTopCard().getName());
-        cpuTopCardImage = (ImageView) findViewById(R.id.cpu_top_card_character_image);
-        cpuTopCardImage.setImageResource(game.getDealer().getTopCard().getImageSource());
-        cpuIntellectText = (TextView) findViewById(R.id.cpu_top_card_intellect_value_text);
-        cpuIntellectText.setText(game.getDealer().getTopCard().getIntellect().toString());
-        cpuLethalityText = (TextView) findViewById(R.id.cpu_top_card_lethality_value_text);
-        cpuLethalityText.setText(game.getDealer().getTopCard().getLethality().toString());
-        cpuMoralityText = (TextView) findViewById(R.id.cpu_top_card_morality_value_text);
-        cpuMoralityText.setText(game.getDealer().getTopCard().getMorality().toString());
-        cpuHowSchwiftyText = (TextView) findViewById(R.id.cpu_top_card_howSchwifty_value_text);
-        cpuHowSchwiftyText.setText(game.getDealer().getTopCard().getHowSchwifty().toString());
-
-        // user
-        userNameText = (TextView) findViewById(R.id.user_name_text);
-        userNameText.setText(game.getPlayer1().getName());
-        userBackplate = (ImageView) findViewById(R.id.user_top_card_backplate);
-        userTopCardName = (TextView) findViewById(R.id.user_top_card_name_text);
-        userTopCardName.setText(game.getPlayer1().getTopCard().getName());
-        userTopCardImage = (ImageView) findViewById(R.id.user_top_card_character_image);
-        userTopCardImage.setImageResource(game.getPlayer1().getTopCard().getImageSource());
-        userIntellectText = (TextView) findViewById(R.id.user_top_card_intellect_value_text);
-        userIntellectText.setText(game.getPlayer1().getTopCard().getIntellect().toString());
-        userLethalityText = (TextView) findViewById(R.id.user_top_card_lethality_value_text);
-        userLethalityText.setText(game.getPlayer1().getTopCard().getLethality().toString());
-        userMoralityText = (TextView) findViewById(R.id.user_top_card_morality_value_text);
-        userMoralityText.setText(game.getPlayer1().getTopCard().getMorality().toString());
-        userHowSchwiftyText = (TextView) findViewById(R.id.user_top_card_howSchwifty_value_text);
-        userHowSchwiftyText.setText(game.getPlayer1().getTopCard().getHowSchwifty().toString());
-
-        // play round and get winner details
-        game.playRound(category);
-        isRoundDraw = game.isRoundDraw();
-        roundWinnerName = game.getRoundWinner().getName();
-        roundWinningCard = game.getRoundWinningCard();
-        roundWinnerScore = game.getRoundWinningScore();
 
         // round category
         roundCategoryText = (TextView) findViewById(R.id.round_category_text);
+
+        // cpu
+        cpuNameText = (TextView) findViewById(R.id.cpu_name_text);
+        cpuBackplate = (ImageView) findViewById(R.id.cpu_top_card_backplate);
+        cpuTopCardName = (TextView) findViewById(R.id.cpu_top_card_name_text);
+        cpuTopCardImage = (ImageView) findViewById(R.id.cpu_top_card_character_image);
+        cpuIntellectText = (TextView) findViewById(R.id.cpu_top_card_intellect_value_text);
+        cpuLethalityText = (TextView) findViewById(R.id.cpu_top_card_lethality_value_text);
+        cpuMoralityText = (TextView) findViewById(R.id.cpu_top_card_morality_value_text);
+        cpuHowSchwiftyText = (TextView) findViewById(R.id.cpu_top_card_howSchwifty_value_text);
+
+        // user
+        userNameText = (TextView) findViewById(R.id.user_name_text);
+        userBackplate = (ImageView) findViewById(R.id.user_top_card_backplate);
+        userTopCardName = (TextView) findViewById(R.id.user_top_card_name_text);
+        userTopCardImage = (ImageView) findViewById(R.id.user_top_card_character_image);
+        userIntellectText = (TextView) findViewById(R.id.user_top_card_intellect_value_text);
+        userLethalityText = (TextView) findViewById(R.id.user_top_card_lethality_value_text);
+        userMoralityText = (TextView) findViewById(R.id.user_top_card_morality_value_text);
+        userHowSchwiftyText = (TextView) findViewById(R.id.user_top_card_howSchwifty_value_text);
+    }
+
+    public void setLayoutViews(){
+        // page title text
+        String roundResultTextStr = "Round " + game.getRoundNumber() + " Result";
+        roundResultText.setText(roundResultTextStr);
+
+        // round category
         String roundCategoryTextStr = "Category:  " + category;
         roundCategoryText.setText(roundCategoryTextStr);
 
-        // win declaration
-        roundWinDeclarationText = (TextView) findViewById(R.id.round_win_declaration_text);
-        String roundWinDeclarationTextStr = roundWinnerName + " won with a score of " + roundWinnerScore.toString() + ".";
-        roundWinDeclarationText.setText(roundWinDeclarationTextStr);
+        // cpu
+        cpuNameText.setText(game.getDealer().getName());
+        cpuTopCardName.setText(game.getDealer().getTopCard().getName());
+        cpuTopCardImage.setImageResource(game.getDealer().getTopCard().getImageSource());
+        cpuIntellectText.setText(game.getDealer().getTopCard().getIntellect().toString());
+        cpuLethalityText.setText(game.getDealer().getTopCard().getLethality().toString());
+        cpuMoralityText.setText(game.getDealer().getTopCard().getMorality().toString());
+        cpuHowSchwiftyText.setText(game.getDealer().getTopCard().getHowSchwifty().toString());
 
-        // check if draw then set colours accordingly
-        if (isRoundDraw){
-            setDrawColours();
-            // draw declaration
-            String roundDrawDeclarationTextStr = "DRAW! Cards added to the pile.";
-            roundWinDeclarationText.setText(roundDrawDeclarationTextStr);
-        } else {
-            setWinColours();
-        }
-
-        // toast catchphrase
-//        Toast.makeText(this, roundWinningCard.getCatchphrase(), Toast.LENGTH_LONG).show();
-
-        toast = Toast.makeText(this, roundWinningCard.getCatchphrase(), Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL, 0, -150);
-        toast.show();
-    }
-
-    public void nextRoundButtonOnClick(View button){
-        toast.cancel();
-        if (!game.isGameWon()){
-            initializeMainActivityIntent();
-        } else {
-            initializeGameWonIntent();
-        }
-    }
-
-    public void initializeMainActivityIntent(){
-        Intent mainActivityIntent = new Intent(ResultActivity.this, MainActivity.class);
-        startActivity(mainActivityIntent);
-    }
-
-    public void initializeGameWonIntent(){
-        Intent gameWonIntent = new Intent(ResultActivity.this, GameWonActivity.class);
-        startActivity(gameWonIntent);
+        // user
+        userNameText.setText(game.getPlayer1().getName());
+        userTopCardName.setText(game.getPlayer1().getTopCard().getName());
+        userTopCardImage.setImageResource(game.getPlayer1().getTopCard().getImageSource());
+        userIntellectText.setText(game.getPlayer1().getTopCard().getIntellect().toString());
+        userLethalityText.setText(game.getPlayer1().getTopCard().getLethality().toString());
+        userMoralityText.setText(game.getPlayer1().getTopCard().getMorality().toString());
+        userHowSchwiftyText.setText(game.getPlayer1().getTopCard().getHowSchwifty().toString());
     }
 
     public void setWinColours(){
@@ -202,6 +196,40 @@ public class ResultActivity extends AppCompatActivity {
         userTopCardName.setTextColor(Color.parseColor("#FFAAAAAA"));
         userNameText.setTextColor(Color.parseColor("#FFAAAAAA"));
         userBackplate.setBackgroundColor(Color.parseColor("#FFAAAAAA"));
+    }
+
+    public void winDeclaration(){
+        // win declaration
+        roundWinDeclarationText = (TextView) findViewById(R.id.round_win_declaration_text);
+        String roundWinDeclarationTextStr = roundWinnerName + " won with a score of " + roundWinnerScore.toString() + ".";
+        roundWinDeclarationText.setText(roundWinDeclarationTextStr);
+    }
+
+    public void toastCatchphrase(){
+        // toast catchphrase
+        toast = Toast.makeText(this, roundWinningCard.getCatchphrase(), Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL, 0, -150);
+        toast.show();
+    }
+
+    public void nextRoundButtonOnClick(View button){
+        toast.cancel();
+
+        if (!game.isGameWon()){
+            initializeMainActivityIntent();
+        } else {
+            initializeGameWonIntent();
+        }
+    }
+
+    public void initializeMainActivityIntent(){
+        Intent mainActivityIntent = new Intent(ResultActivity.this, MainActivity.class);
+        startActivity(mainActivityIntent);
+    }
+
+    public void initializeGameWonIntent(){
+        Intent gameWonIntent = new Intent(ResultActivity.this, GameWonActivity.class);
+        startActivity(gameWonIntent);
     }
 
 }
