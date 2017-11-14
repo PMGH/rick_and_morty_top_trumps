9d2f847afd97ae7c7653e92c2770ad835645489c
Peter McCready <petermccready2@gmail.com>
2017-11-14 18:44:06 +0000

    Adds comments to ResultActivity

--- app/src/main/java/com/example/peter/toptrumps/ResultActivity.java
+++ app/src/main/java/com/example/peter/toptrumps/ResultActivity.java
@@ -40,8 +40,6 @@ public class ResultActivity extends AppCompatActivity {
     ImageView userBackplate;
 
     Game game;
-    Card userCard;
-    Card cpuCard;
     Card roundWinningCard;
     String category;
     String roundWinnerName;
@@ -58,8 +56,6 @@ public class ResultActivity extends AppCompatActivity {
 
         // setup game
         game = Game.getInstance();
-        userCard = game.getPlayer1().getTopCard();
-        cpuCard = game.getDealer().getTopCard();
 
         // setup views
         getLayoutViews();
@@ -72,6 +68,7 @@ public class ResultActivity extends AppCompatActivity {
         roundWinningCard = game.getRoundWinningCard();
         roundWinnerScore = game.getRoundWinningScore();
 
+        // declare win
         winDeclaration();
 
         // check if draw then set colours accordingly
@@ -102,13 +99,13 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     public void getLayoutViews(){
-        // page title text
+        // get page title text
         roundResultText = (TextView) findViewById(R.id.round_result_title_text);
 
-        // round category
+        // get round category
         roundCategoryText = (TextView) findViewById(R.id.round_category_text);
 
-        // cpu
+        // get cpu views
         cpuNameText = (TextView) findViewById(R.id.cpu_name_text);
         cpuBackplate = (ImageView) findViewById(R.id.cpu_top_card_backplate);
         cpuTopCardName = (TextView) findViewById(R.id.cpu_top_card_name_text);
@@ -118,7 +115,7 @@ public class ResultActivity extends AppCompatActivity {
         cpuMoralityText = (TextView) findViewById(R.id.cpu_top_card_morality_value_text);
         cpuHowSchwiftyText = (TextView) findViewById(R.id.cpu_top_card_howSchwifty_value_text);
 
-        // user
+        // get user views
         userNameText = (TextView) findViewById(R.id.user_name_text);
         userBackplate = (ImageView) findViewById(R.id.user_top_card_backplate);
         userTopCardName = (TextView) findViewById(R.id.user_top_card_name_text);
@@ -130,15 +127,15 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     public void setLayoutViews(){
-        // page title text
+        // set page title text
         String roundResultTextStr = "Round " + game.getRoundNumber() + " Result";
         roundResultText.setText(roundResultTextStr);
 
-        // round category
+        // set round category
         String roundCategoryTextStr = "Category:  " + category;
         roundCategoryText.setText(roundCategoryTextStr);
 
-        // cpu
+        // set cpu views
         cpuNameText.setText(game.getDealer().getName());
         cpuTopCardName.setText(game.getDealer().getTopCard().getName());
         cpuTopCardImage.setImageResource(game.getDealer().getTopCard().getImageSource());
@@ -147,7 +144,7 @@ public class ResultActivity extends AppCompatActivity {
         cpuMoralityText.setText(game.getDealer().getTopCard().getMorality().toString());
         cpuHowSchwiftyText.setText(game.getDealer().getTopCard().getHowSchwifty().toString());
 
-        // user
+        // set user views
         userNameText.setText(game.getPlayer1().getName());
         userTopCardName.setText(game.getPlayer1().getTopCard().getName());
         userTopCardImage.setImageResource(game.getPlayer1().getTopCard().getImageSource());
@@ -158,24 +155,28 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     public void setWinColours(){
-        // cpu colours
+        // set cpu colours
         if (roundWinnerName.equals(game.getDealer().getName())){
+            // on win
             cpuNameText.setTextColor(Color.parseColor("#ff669900"));
             cpuBackplate.setBackgroundColor(Color.parseColor("#ff669900"));
             cpuTopCardName.setTextColor(Color.parseColor("#ff669900"));
         } else {
+            // on lose
             cpuNameText.setTextColor(Color.parseColor("#ffcc0000"));
             cpuBackplate.setBackgroundColor(Color.parseColor("#ffcc0000"));
             cpuBackplate.setPadding(2, 2, 2, 2);
             cpuTopCardName.setTextColor(Color.parseColor("#ffcc0000"));
         }
 
-        // user colours
+        // set user colours
         if (roundWinnerName.equals(game.getPlayer1().getName())){
+            // on win
             userNameText.setTextColor(Color.parseColor("#ff669900"));
             userBackplate.setBackgroundColor(Color.parseColor("#ff669900"));
             userTopCardName.setTextColor(Color.parseColor("#ff669900"));
         } else {
+            // on lose
             userNameText.setTextColor(Color.parseColor("#ffcc0000"));
             userBackplate.setBackgroundColor(Color.parseColor("#ffcc0000"));
             userBackplate.setPadding(2, 2, 2, 2);
@@ -184,22 +185,22 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     public void setDrawColours(){
-        // draw declaration
+        // set draw declaration
         roundWinDeclarationText.setTextColor(Color.parseColor("#FFAAAAAA"));
 
-        // cpu colours
+        // set cpu colours
         cpuTopCardName.setTextColor(Color.parseColor("#FFAAAAAA"));
         cpuNameText.setTextColor(Color.parseColor("#FFAAAAAA"));
         cpuBackplate.setBackgroundColor(Color.parseColor("#FFAAAAAA"));
 
-        // user colours
+        // set user colours
         userTopCardName.setTextColor(Color.parseColor("#FFAAAAAA"));
         userNameText.setTextColor(Color.parseColor("#FFAAAAAA"));
         userBackplate.setBackgroundColor(Color.parseColor("#FFAAAAAA"));
     }
 
     public void winDeclaration(){
-        // win declaration
+        // get/set win declaration
         roundWinDeclarationText = (TextView) findViewById(R.id.round_win_declaration_text);
         String roundWinDeclarationTextStr = roundWinnerName + " won with a score of " + roundWinnerScore.toString() + ".";
         roundWinDeclarationText.setText(roundWinDeclarationTextStr);
